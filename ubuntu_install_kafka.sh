9941f468b849b90aa655949156ba99c51b77173a
Brittany Chiu <bc.chiu12@gmail.com>
2018-05-29 13:15:31 -0400

    kafka

---
+++ ubuntu_install_kafka.sh
@@ -0,0 +1,27 @@
+# Install java
+sudo apt-get install -y default-jre
+
+# Install zookeeper
+sudo apt-get install -y zookeeperd
+
+# Add Kafka user
+sudo adduser --system --no-create-home --disabled-password --disabled-login kafka
+
+# Install Kafka Server
+cd ~
+wget "http://www-eu.apache.org/dist/kafka/1.0.1/kafka_2.12-1.0.1.tgz"
+
+# Create a directory for extracting Kafka
+sudo mkdir /opt/kafka
+sudo tar -xvzf kafka_2.12-1.0.1.tgz --directory /opt/kafka --strip-components 1
+
+# Kafka persists data to disk so we will now make a directory for it.
+sudo mkdir /var/lib/kafka
+sudo mkdir /var/lib/kafka/data
+
+# Ensure Permission of Directories
+sudo chown -R kafka:nogroup /opt/kafka
+sudo chown -R kafka:nogroup /var/lib/kafka
+
+# Start the Kafka server 
+sudo /opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties
