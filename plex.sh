86d1afbc22724fa0bf0fa731f1c583c363e46d92
Brittany Chiu <bcc5160@psu.edu>
2018-06-25 22:23:01 -0400

    plex

---
+++ plex.sh
@@ -0,0 +1,10 @@
+# Update the system
+sudo apt-get update && sudo apt-get upgrade
+
+# Install Plex
+wget https://downloads.plex.tv/plex-media-server/1.5.5.3634-995f1dead/plexmediaserver_1.5.5.3634-995f1dead_amd64.deb
+sudo dpkg -i plexmediaserver*.deb
+
+# Start Plex
+sudo systemctl enable plexmediaserver.service
+sudo systemctl start plexmediaserver.service
