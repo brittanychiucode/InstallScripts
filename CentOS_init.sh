002df968249b019a2a008a5f498157081643d418
Brittany Chiu <bc.chiu12@gmail.com>
2022-06-10 23:40:16 -0400

    add java, tomcat, maven to centos

--- CentOS_init.sh
+++ CentOS_init.sh
@@ -24,6 +24,24 @@ wget https://bootstrap.pypa.io/get-pip.py
 python get-pip.py
 rm get-pip.py
 
+# Install Maven
+sudo yum install maven
+
+# Install Java
+sudo yum install java-11-openjdk-devel
+
+# Install Tomcat: https://linuxize.com/post/how-to-install-tomcat-9-on-centos-7/
+# add tomcat user will run tomcat
+sudo useradd -m -U -d /opt/tomcat -s /bin/false tomcat
+# download and install 
+cd /tmp
+wget https://www-eu.apache.org/dist/tomcat/tomcat-9/v9.0.27/bin/apache-tomcat-9.0.27.tar.gz
+tar -xf apache-tomcat-9.0.27.tar.gz
+sudo mv apache-tomcat-9.0.27 /opt/tomcat/
+sudo ln -s /opt/tomcat/apache-tomcat-9.0.27 /opt/tomcat/latest
+sudo chown -R tomcat: /opt/tomcat
+sudo sh -c 'chmod +x /opt/tomcat/latest/bin/*.sh'
+
 # Install Ansible
 # sudo yum -y install epel-release
 # sudo yum -y install ansible
